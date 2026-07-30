c387c047f81bf49e56c8d71419dc32d1e3c1094a
AndreySantos <santosandrey146@gmail.com>
2026-07-30 08:05:51 -0300

    Atualizando configuração de CORS.

--- GeradorDeQuestoes/src/main/java/com/Projeto/GeradorDeQuestoes/configs/SecurityConfig.java
+++ GeradorDeQuestoes/src/main/java/com/Projeto/GeradorDeQuestoes/configs/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -32,6 +33,7 @@ public class SecurityConfig {
         SecurityFilter securityFilter = new SecurityFilter(tokenService, usuarioRepository);
 
         return http
+        .cors(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
